6b4912a4942a194de67802eb0a9925ddf27f5fe1
3a532008 <isme1788@gmail.com>
2018-10-19 13:59:57 +0800

    建立tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+
+    <!-- CSS 及 JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: 曾敏華
+ * Date: 2018/10/19
+ * Time: 下午 01:58
+ */
